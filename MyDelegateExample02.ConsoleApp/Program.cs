c8e9eb627fe7509d21d692b06dda1d9a801cd25b
Avinash M <avinashm201993@gmail.com>
2018-08-28 23:46:04 -0500

    Implemented multicasting delegate

--- MyDelegateExample02.ConsoleApp/Program.cs
+++ MyDelegateExample02.ConsoleApp/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace MyDelegateExample02.ConsoleApp
 {
+    public delegate void PersonDetails(string info);
+
     class Program
     {
+
+        static void City(string name)
+        {
+            Console.WriteLine($"{name} lives in US.");
+        }
+
+        static void Age(string name)
+        {
+            Console.WriteLine($"{name} is 12 years old");
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            PersonDetails pd = Age;
+            pd += City;    // multicasting delegate
+
+            pd(name);
+
         }
     }
 }
